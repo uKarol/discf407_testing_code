4e56a47f7cdc8b77a28a7a5f10b681711e6d8794
uKarol <karol.ujda@gmail.com>
2025-04-06 14:15:51 +0200

    add exemple with usb otg vcp

---
+++ usb_virtual_com_tests.c/Core/Inc/buffer_config.h
@@ -0,0 +1,14 @@
+/*
+ * buffer_config.h
+ *
+ *  Created on: Apr 3, 2025
+ *      Author: Karol
+ */
+
+#ifndef INC_BUFFER_CONFIG_H_
+#define INC_BUFFER_CONFIG_H_
+
+#define MAX_BUFFER_SIZE 2048
+#define MAX_BUFFER_NUMBER 2
+
+#endif /* INC_BUFFER_CONFIG_H_ */
